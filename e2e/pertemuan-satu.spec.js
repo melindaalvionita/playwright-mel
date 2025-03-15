8d509ec32d0f98facfd7bc752b63859e605fadee
melindaalvionita <melindalvionita16@gmail.com>
2025-03-15 07:17:39 +0700

    tambah testcase login

--- e2e/pertemuan-satu.spec.js
+++ e2e/pertemuan-satu.spec.js
@@ -1,8 +1,19 @@
 import { test, expect } from '@playwright/test';
-
+ 
 test('has title', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
+  const username = 'standars_user';
+  const password = 'secret_sauce';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Swag Labs');
+  const inputUsername = page.locator('#user-name');
+  await inputUsername.fill(username);
+  await expect(inputUsername).toHaveValue(username);
+
+  const inputPassword = page.locator('#password');
+  await inputPassword.fill(password);
+  await expect(inputPassword).toHaveValue(password);
+
+  const buttonLogin = page.locator('#login-button');
+  await buttonLogin.click();
+
 });
